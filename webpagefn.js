3ca6e2423592804b2b19c55584cbbb6f0c2e4eb4
unknown <Administrator@cse-rd33.SAEC.EDU>
2024-12-31 16:00:42 +0530

    webpagefn

--- webpagefn.js
+++ webpagefn.js
@@ -1,8 +1,9 @@
 var num1=document.getElementById("num1");
 var num2=document.getElementById("num2");
 var result=document.getElementById("result");
-
+SubmitEvent(){
 var box1=num1.value;
 var box2=num2.value;
 var total=Number(box1)+Number(box2);
 console.log(total);
+}
