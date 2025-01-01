3322b013b1da04efe34b3e07b65a8e05df9524ee
Arun-clover <arunkumarkarna.2104@gmail.com>
2025-01-01 11:54:03 +0530

    webpagefn

--- webpagefn.js
+++ webpagefn.js
@@ -7,3 +7,21 @@ var box2=num2.value;
 var total=Number(box1)+Number(box2);
 console.log(total);
 }
+function product(){
+var box1=num1.value;
+var box2=num2.value;
+var total=Number(box1)*Number(box2);
+console.log(total);
+}
+function division(){
+var box1=num1.value;
+var box2=num2.value;
+var total=Number(box1)/Number(box2);
+console.log(total);
+}
+function sub(){
+var box1=num1.value;
+var box2=num2.value;
+var total=Number(box1)-Number(box2);
+console.log(total);
+}
