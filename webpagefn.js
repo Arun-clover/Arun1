74de6ff9e768aecc858a28f19c54f584329c28e2
Arun-clover <arunkumarkarna.2104@gmail.com>
2025-01-01 11:39:47 +0530

    webpagefn

--- webpagefn.js
+++ webpagefn.js
@@ -1,7 +1,7 @@
 var num1=document.getElementById("num1");
 var num2=document.getElementById("num2");
 var result=document.getElementById("result");
-Sum(){
+function sum(){
 var box1=num1.value;
 var box2=num2.value;
 var total=Number(box1)+Number(box2);
